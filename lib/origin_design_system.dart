ba4e45418cd5acb8e83db2863fc2d96d42873310
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-20 17:42:18 -0300

    Update exports

--- lib/origin_design_system.dart
+++ lib/origin_design_system.dart
@@ -2,4 +2,14 @@
 library origin_design_system;
 
 //atoms
+export 'src/atoms/colors.dart';
 export 'src/atoms/origin_icon.dart';
+export 'src/atoms/origin_logo.dart';
+export 'src/atoms/spacing.dart';
+export 'src/atoms/text_styles.dart';
+
+//molecules
+export 'src/molecules/origin_app_bar.dart';
+
+//organisms
+export 'src/organisms/origin_page.dart';
