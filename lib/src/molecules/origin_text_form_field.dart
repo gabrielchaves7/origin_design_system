d31d99bedd08f7e86ec76dd3e4488aea5993a9c0
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-21 21:23:06 -0300

    Added width and height constraints to text form field

--- lib/src/molecules/origin_text_form_field.dart
+++ lib/src/molecules/origin_text_form_field.dart
@@ -36,62 +36,71 @@ class OriginTextField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final hasError = errorText != null;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: hasError ? const TextStyle(color: OriginColors.red700) : null,
-        ).description,
-        const SizedBox(
-          height: OriginSpacing.x,
-        ),
-        TextField(
-          onChanged: (value) => onChanged?.call(value.replaceAll(',', '')),
-          keyboardType: textInputType,
-          inputFormatters: inputFormatters,
-          style: OriginTextStyles.headingSmall.merge(
-            TextStyle(
-              color: hasError ? OriginColors.red700 : OriginColors.blueGray600,
+    return Container(
+      constraints: const BoxConstraints(
+        maxWidth: 320,
+        minWidth: 232,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style:
+                hasError ? const TextStyle(color: OriginColors.red700) : null,
+          ).description,
+          const SizedBox(
+            height: OriginSpacing.x,
+          ),
+          TextField(
+            onChanged: (value) => onChanged?.call(value.replaceAll(',', '')),
+            keyboardType: textInputType,
+            inputFormatters: inputFormatters,
+            style: OriginTextStyles.headingSmall.merge(
+              TextStyle(
+                color:
+                    hasError ? OriginColors.red700 : OriginColors.blueGray600,
+              ),
+            ),
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: OriginSpacing.xs),
+              prefixIconConstraints: const BoxConstraints(
+                maxHeight: 24,
+                maxWidth: 40,
+              ),
+              prefixIcon: leadingIconPath != null
+                  ? Padding(
+                      padding: const EdgeInsetsDirectional.only(
+                        start: OriginSpacing.xs,
+                        end: OriginSpacing.xs,
+                      ),
+                      child: OriginIcon(
+                        iconPath: leadingIconPath!,
+                        colorFilter: hasError
+                            ? const ColorFilter.mode(
+                                Colors.red, BlendMode.srcIn)
+                            : null,
+                      ),
+                    )
+                  : null,
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: OriginColors.blueGray50),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: OriginColors.brandColorPrimary),
+              ),
+              focusedErrorBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: OriginColors.red700),
+              ),
+              errorBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: OriginColors.red700),
+              ),
+              errorText: errorText,
             ),
           ),
-          decoration: InputDecoration(
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: OriginSpacing.xs),
-            prefixIconConstraints: const BoxConstraints(
-              maxHeight: 24,
-              maxWidth: 40,
-            ),
-            prefixIcon: leadingIconPath != null
-                ? Padding(
-                    padding: const EdgeInsetsDirectional.only(
-                      start: OriginSpacing.xs,
-                      end: OriginSpacing.xs,
-                    ),
-                    child: OriginIcon(
-                      iconPath: leadingIconPath!,
-                      colorFilter: hasError
-                          ? const ColorFilter.mode(Colors.red, BlendMode.srcIn)
-                          : null,
-                    ),
-                  )
-                : null,
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: OriginColors.blueGray50),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: OriginColors.brandColorPrimary),
-            ),
-            focusedErrorBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: OriginColors.red700),
-            ),
-            errorBorder: const OutlineInputBorder(
-              borderSide: BorderSide(color: OriginColors.red700),
-            ),
-            errorText: errorText,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
