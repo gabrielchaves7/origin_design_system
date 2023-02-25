615c37e462ddbe0d601fae9449895595851ea6de
Gabriel Chaves <gabrielbum@gmail.com>
2023-02-25 17:31:51 -0300

    Removed widgetbook

--- lib/src/widgetbook/main.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
-import 'package:origin_design_system/src/widgetbook/widgetbook.widgetbook.dart';
-
-Future<void> main() async {
-  runApp(const HotReload());
-}
